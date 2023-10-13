0295b7a600ab5a3578490785971dcc171a2aca16
MenendezON <menendezon@gmail.com>
2023-10-13 13:56:32 +0000

    Add the factorial test

--- spec/solve_rspec.rb
+++ spec/solve_rspec.rb
@@ -3,5 +3,18 @@ require_relative '../solve'
 
 describe Solve do
   subject { Solve.new }
+  context 'factorial' do
+    it 'returns 1 when the input is 0' do
+      expect(subject.factorial(0)).to eq(1)
+    end
 
+    it 'returns the correct factorial for positive integers' do
+      expect(subject.factorial(5)).to eq(120)
+      expect(subject.factorial(7)).to eq(5040)
+    end
+
+    it 'raises an exception for negative integers' do
+      expect { subject.factorial(-3) }.to raise_error(ArgumentError)
+    end
+  end
 end
