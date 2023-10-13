12eb0d9355be335e697ecafefe5979cd748dce9e
MenendezON <menendezon@gmail.com>
2023-10-13 13:57:35 +0000

    Create the factorial function

--- solve.rb
+++ solve.rb
@@ -1,3 +1,11 @@
 class Solve
-
+  def factorial(number)
+    if number.negative?
+      raise ArgumentError, 'Factorial is not defined for negative integers.'
+    elsif number.zero
+      1
+    else
+      (1..number).reduce(:*)
+    end
+  end
 end
